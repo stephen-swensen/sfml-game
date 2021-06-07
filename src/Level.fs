288e4f326f776ddae56d26a84d4aa5de33c15902
Stephen Swensen <stephen.swensen@gmail.com>
2021-06-07 00:43:12 -0400

    levels

---
+++ src/Level.fs
@@ -0,0 +1,26 @@
+namespace Swensen.SFML.Game
+
+type Level =
+    { EnemyCount: int
+      EnemyDirections: (Direction option) list
+      StartText: string }
+
+module Level =
+    let loadAll () =
+        [ { EnemyCount = 4
+            EnemyDirections = [ None ]
+            StartText = "Circles abound...\nYou.\nMust.\nEat them all!!" }
+          { EnemyCount = 8
+            EnemyDirections =
+                [ Some Up
+                  Some Down
+                  Some Left
+                  Some Right ]
+            StartText = "They've learned and they've learned fast...\nTry to keep up!!" }
+          { EnemyCount = 16
+            EnemyDirections =
+                [ Some Up
+                  Some Down
+                  Some Left
+                  Some Right ]
+            StartText = "Things are getting a little hectic\naround here!" } ]
